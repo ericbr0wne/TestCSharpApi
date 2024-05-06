dca8e3c7e33c72d8d91c1d55e8e692cb4c773433
Eric Browne <ericbrowne@hotmail.com>
2024-05-06 22:23:30 +0200

    added psw test

--- Backend/unit-tests/UtilsTest.cs
+++ Backend/unit-tests/UtilsTest.cs
@@ -2,30 +2,26 @@ using Xunit;
 namespace WebApp;
 public class UtilsTest
 {
-    /*[Fact]
-    public void TestSumInt()
+
+
+    //Metod 1
+    [Theory]
+    [InlineData("psw123", false)] // Bad password
+    [InlineData("Psw12345!", true)] // Good password
+    public void IsPasswordGoodEnoughTest(string password, bool expectedResult)
     {
-        Assert.Equal(12, Utils.SumInts(7, 5));
-        Assert.Equal(-3, Utils.SumInts(6, -9));
+        bool result = Utils.IsPasswordGoodEnough(password);
 
-    }*/
+        Assert.Equal(expectedResult, result);
+    
+    }
 
-[Fact]
-[Theory]
-public void IsPasswordGoodEnoughTest()
-{
-    string badPassword = "psw123"; 
-    string goodPassword = "Psw12345!";
 
-    bool isBadPasswordGoodEnough = Utils.IsPasswordGoodEnough(badPassword);
-    bool isGoodPasswordGoodEnough = Utils.IsPasswordGoodEnough(goodPassword);
+    //Metod 2
 
-    Assert.False(isBadPasswordGoodEnough);
-    Assert.True(isGoodPasswordGoodEnough);
-}
 
-}
 
+    //Metod 3
     /*[Fact]
     public void TestCreateMockUsers(){
        var read = File.ReadAllText(Path.Combine("json/mock-users.json"));  
@@ -42,24 +38,34 @@ public void IsPasswordGoodEnoughTest()
        Log("mockUserNotInDB.Length", mockUserNotInDB.Length );
 
 
-var result = Utils.CreateMockUsers();
-//Console.WriteLine("TEST:", mockUserNotInDB);
-//Console.WriteLine("METHOD:", result);
-//Assert the same length
-Assert.Equal(mockUserNotInDB.Length, result.Length);
+    var result = Utils.CreateMockUsers();
+    //Console.WriteLine("TEST:", mockUserNotInDB);
+    //Console.WriteLine("METHOD:", result);
+    //Assert the same length
+    Assert.Equal(mockUserNotInDB.Length, result.Length);
 
-//Check equivalency for each user
-for (int i = 0; i < result.Length; i++){
+    //Check equivalency for each user
+    for (int i = 0; i < result.Length; i++){
     //Assert.Equivalent(mockUserNotInDB[i], result[i]);
 
    //Console.WriteLine("result[i]", result[i]);
    //Console.WriteLine("mockUsersNotInDB[i]", result[i]);
-}
+    }
 
        //Assert.Equivalent(mockUserNotInDB, Utils.CreateMockUsers());
 
       /* foreach(var user in usersInDB)
        
         mockUsers = mockUsers.Filter(mockUser => mockUser.email != user.email);
-*/
+    */
 
+
+
+
+    //Metod 4
+
+
+
+    //Metod 5
+
+}
