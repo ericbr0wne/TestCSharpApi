a889ea87f70748eb28d22a81af5b55950a2ef9ef
Eric Browne <ericbrowne@hotmail.com>
2024-05-21 15:14:05 +0200

    product-description working!

--- ui-tests/specs/step_definitions/product-description.js
+++ ui-tests/specs/step_definitions/product-description.js
@@ -1,2 +1,14 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
+Given('that I is on out on the product page', () => {
+    cy.visit("/products");
+});
+
+When('I clicked category {string}', (category) => {
+    cy.get('#categories').select(category);
+});
+
+Then('the product {string} should be shown with the description {string}', (productName) => {
+    cy.get('.product .name').contains(productName);
+
+});
